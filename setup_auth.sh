def5d06ab3cce61d821f1f602b7d7c6d4fe53d62
Rejka Permana <rejkamuhammad@gmail.com>
2025-04-23 17:50:30 +0700

    fix setup on routes

--- setup_auth.sh
+++ setup_auth.sh
@@ -19,7 +19,8 @@ read -p "Do you want to add flash message notifications? (y/n) " add_flash
 
 # Inject resource :registration route after resource :session in config/routes.rb
 sed -i '/resource :session/a \  resource :registration, only: %i[new create]' config/routes.rb
-sed -i '/get "home/index"/a \  root "home#index"' config/routes.rb
+# Add root route after home/index
+sed -i '/get "home\/index"/a \  root "home#index"' config/routes.rb
 echo "✅ update routes"
 
 curl -s https://raw.githubusercontent.com/jusondac/lazy_script/refs/heads/master/auth_setup/sessions_controller.rb > app/controllers/sessions_controller.rb
